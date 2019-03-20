de70616358e35baa123e9e068b407beb968ab0a7
Guillaume Ley <guillaumeley@MacBook-Pro-de-Guillaume.local>
2019-03-20 14:10:22 +0100

    new project

---
+++ app/index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- GOOGLE FONT -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <!-- GITHUB -->
+    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <!-- BULMA -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
+    <!-- STYLE CSS -->
+    <link rel="stylesheet" href="src/assets/sass/style.scss">
+    <title>GameProject</title>
+</head>
+<body>
+    <section class="hero is-info is-fullheight">
+        <div class="hero-head">
+        <nav class="navbar"> 
+
+        <div class="container">
+            <div class="navbar-brand">
+                <span class="navbar-burger burger" data-target="navbarMenuHeroB">
+                <span></span>
+                <span></span>
+                <span></span>
+                </span>
+            </div>
+            <div id="navbarMenuHeroB" class="navbar-menu">
+                <div class="navbar-end">
+                <a class="navbar-item is-active">
+                    Home
+                </a>
+                <a class="navbar-item">
+                    Examples
+                </a>
+                <a class="navbar-item">
+                    Documentation
+                </a>
+                </div>
+            </div>
+            </div>
+        </nav>
+        </div>
+
+        <div class="hero-body">
+        <div class="container has-text-centered">
+            <p class="title is-1">
+            Select your GAME
+            </p>
+            <p class="subtitle is-4">
+            ESD GAME
+            </p>
+        </div>
+        </div>
+    </section>
+    <div class="gameline1">
+        <div class="columns is-centered">            
+          <div class="column is-one-fifth">
+            <div class="card">
+                <div class="card-image">
+                    <figure class="image is-4by3">
+                        <img src="img/Stargazing.png" alt="Placeholder image">
+                    </figure>
+                </div><!-- /CARD IMAGE -->
+                
+                <div class="card-content">
+                    <div class="media">    
+                        <div class="media-content">
+                            <p class="title is-4">Stargazing</p>
+                        </div><!-- /MEDIA CONTENT -->
+                    </div>
+                
+                    <div class="content">
+                      Vous jouez un astronaute dont la planète
+                      natale à été détruite sa planète natale le laissant perdu dans le fin fond du cosmos, seul
+                      avec son vaisseau. Le but est de parcourir les étoiles à travers l’espace afin de trouver une
+                      planète habitable.
+                    </div><!-- /CONTENT -->
+                </div><!-- /CARD CONTENT -->
+            </div><!-- /CARD -->
+          </div><!-- /COLUMN FIFTH -->
+
+
+
+          <div class="column is-one-fifth">
+            <div class="card">
+                <div class="card-image">
+                    <figure class="image is-4by3">
+                        <img src="img/Pain.png" alt="Placeholder image">
+                    </figure>
+                </div><!-- /CARD IMAGE -->
+                
+                <div class="card-content">
+                    <div class="media">    
+                        <div class="media-content">
+                            <p class="title is-4">Pain</p>
+                        </div><!-- /MEDIA CONTENT -->
+                    </div>
+                
+                    <div class="content">
+                      Le personnage principal de ce jeu,
+                      au fil de sa progression fera la connaissance de ces conflits, sera plongé dans
+                      la définition du desespoir et essaiera de surmonter cela.
+                    </div><!-- /CONTENT -->
+                </div><!-- /CARD CONTENT -->
+            </div><!-- /CARD -->
+          </div><!-- /COLUMN FIFTH -->
+
+          
+
+          <div class="column is-one-fifth">
+            <div class="card">
+                <div class="card-image">
+                    <figure class="image is-4by3">
+                        <img src="img/game-1.jpg" alt="Placeholder image">
+                    </figure>
+                </div><!-- /CARD IMAGE -->
+                
+                <div class="card-content">
+                    <div class="media">    
+                        <div class="media-content">
+                            <p class="title is-4">Assassin's Creed Syndicate</p>
+                            <p class="subtitle is-6">June 13, 2017</p>
+                        </div><!-- /MEDIA CONTENT -->
+                    </div>
+                
+                    <div class="content">
+                        Defeating the corrupt tyrants entrenched there will require not only strength, but leadership.
+                    </div><!-- /CONTENT -->
+                </div><!-- /CARD CONTENT -->
+            </div><!-- /CARD -->
+          </div><!-- /COLUMN FIFTH -->
+        </div><!-- /COLUMN CENTERED -->
+      </div><!-- /GAME LINE1 -->
+
+
+
+
+
+      <div class="gameline2">
+        <div class="columns is-centered">            
+          <div class="column is-one-fifth">
+            <div class="card">
+                <div class="card-image">
+                    <figure class="image is-4by3">
+                        <img src="img/game-1.jpg" alt="Placeholder image">
+                    </figure>
+                </div><!-- /CARD IMAGE -->
+                
+                <div class="card-content">
+                    <div class="media">    
+                        <div class="media-content">
+                            <p class="title is-4">Assassin's Creed Syndicate</p>
+                            <p class="subtitle is-6">June 13, 2017</p>
+                        </div><!-- /MEDIA CONTENT -->
+                    </div>
+                
+                    <div class="content">
+                        Defeating the corrupt tyrants entrenched there will require not only strength, but leadership.
+                    </div><!-- /CONTENT -->
+                </div><!-- /CARD CONTENT -->
+            </div><!-- /CARD -->
+          </div><!-- /COLUMN FIFTH -->
+
+
+
+          <div class="column is-one-fifth">
+            <div class="card">
+                <div class="card-image">
+                    <figure class="image is-4by3">
+                        <img src="img/game-1.jpg" alt="Placeholder image">
+                    </figure>
+                </div><!-- /CARD IMAGE -->
+                
+                <div class="card-content">
+                    <div class="media">    
+                        <div class="media-content">
+                            <p class="title is-4">Assassin's Creed Syndicate</p>
+                            <p class="subtitle is-6">June 13, 2017</p>
+                        </div><!-- /MEDIA CONTENT -->
+                    </div>
+                
+                    <div class="content">
+                        Defeating the corrupt tyrants entrenched there will require not only strength, but leadership.
+                    </div><!-- /CONTENT -->
+                </div><!-- /CARD CONTENT -->
+            </div><!-- /CARD -->
+          </div><!-- /COLUMN FIFTH -->
+
+          
+
+          <div class="column is-one-fifth">
+            <div class="card">
+                <div class="card-image">
+                    <figure class="image is-4by3">
+                        <img src="img/game-1.jpg" alt="Placeholder image">
+                    </figure>
+                </div><!-- /CARD IMAGE -->
+                
+                <div class="card-content">
+                    <div class="media">    
+                        <div class="media-content">
+                            <p class="title is-4">Assassin's Creed Syndicate</p>
+                            <p class="subtitle is-6">June 13, 2017</p>
+                        </div><!-- /MEDIA CONTENT -->
+                    </div>
+                
+                    <div class="content">
+                        Defeating the corrupt tyrants entrenched there will require not only strength, but leadership.
+                    </div><!-- /CONTENT -->
+                </div><!-- /CARD CONTENT -->
+            </div><!-- /CARD -->
+          </div><!-- /COLUMN FIFTH -->
+        </div><!-- /COLUMN CENTERED -->
+      </div><!-- /GAME LINE2 -->
+
+    <button><a href="increase_vote.php?game=macron">Voter</a></button>
+</body>
+</html>
+
+<br>
+<?php 
+
+$game = "macron";
+// Connect to database
+$db = new PDO('mysql:host=localhost;dbname=game_vote', 'root','root');
+
+//Request to database (get nb_vote)
+$q = $db->prepare("SELECT  * FROM votes WHERE slug_game = :game");
+$q->bindParam(":game", $game);
+$q->execute();
+
+$data = $q->fetch(PDO::FETCH_ASSOC);
+
+echo $data["nb_votes"] . " votes";
+
+?>
